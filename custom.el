a5efff4d27491cfe5373139b49b7764c831c06d8
Aurélien Bottazini <aurelien.bottazini@gmail.com>
2020-10-24 21:02:00 +0200

    cleanup

--- custom.el
+++ custom.el
@@ -28,7 +28,7 @@
  '(dumb-jump-mode t)
  '(electric-pair-mode nil)
  '(electric-quote-string t)
- '(evil-undo-system 'undo-redo)
+ '(evil-undo-system nil)
  '(fci-rule-color "#383838")
  '(flycheck-standard-error-navigation nil)
  '(frame-background-mode 'light)
