fc9aefe50eefef78d4d072a147d4bdf10a0e0b49
Aurélien Bottazini <aurelien.bottazini@gmail.com>
2020-11-07 15:49:08 +0100

    fix fzf

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(ansi-color-names-vector
    ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
  '(ansi-term-color-vector
-   [unspecified "black" "red" "green" "yellow" "blue" "brightmagenta" "cyan" "white"] t)
+   [term "black" "red" "green" "yellow" "blue" "brightmagenta" "cyan" "white"] t)
  '(blink-cursor-mode nil)
  '(blink-matching-paren 'jump-offscreen)
  '(coffee-tab-width 2)
