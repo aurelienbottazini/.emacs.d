d88afd6ee2f1d05ec624327d40d3f20b27c9dc00
Aurélien Bottazini <aurelien.bottazini@gmail.com>
2020-12-01 17:24:31 +0100

    todo keywords

--- init.el
+++ init.el
@@ -694,7 +694,7 @@ cons cell (regexp . minor-mode)."
 (define-key my-keys-minor-mode-map "\C-ci" 'counsel-org-goto)
 
 (setq org-todo-keywords
-      '((sequence "TODO(t)" "STARTED(s)" "WAITING(w)" "|" "CANCELED(canceled)" "DONE(d!)")))
+      '((sequence "TODO(t)" "STARTED(s!)" "WAITING(w@/!)" "|" "DONE(d!)" "CANCELED(canceled@)")))
 
 (use-package deft
  :bind (:map my-keys-minor-mode-map
