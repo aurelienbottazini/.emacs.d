aeb66d271753ece9b5f743645df2194f859d466f
Aurélien Bottazini <aurelien.bottazini@gmail.com>
2021-11-04 21:53:15 +0100

    style: doom-zenburn

--- init.el
+++ init.el
@@ -1313,7 +1313,7 @@ This command switches to browser."
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-gruvbox t)
+  (load-theme 'doom-zenburn t)
 
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
