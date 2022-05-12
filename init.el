14b98f385058a9cdecd7d743c95fae7ff8f2d922
Aurélien Bottazini <aurelien.bottazini@gmail.com>
2022-05-12 17:25:47 +0200

    zenburn colors

--- init.el
+++ init.el
@@ -339,49 +339,54 @@
   (key-chord-define evil-insert-state-map  "jk" 'evil-normal-state))
 
 (use-package zenburn-theme
-:custom-face
- (evil-search-highlight-persist-highlight-face ((t (:background "#f8f893" :foreground "black"))))
- (font-lock-comment-face ((t (:foreground "#7F9F7F" :slant italic))))
- (hi-aquamarine ((t (:background "aquamarine" :foreground "black"))))
- (hi-salmon ((t (:background "light salmon" :foreground "black"))))
- (hlt-property-highlight ((t (:background "Wheat" :foreground "black"))))
- (hlt-regexp-level-1 ((t (:background "#FA6CC847FFFF" :foreground "black"))))
- (hlt-regexp-level-2 ((t (:background "#C847FFFFE423" :foreground "black"))))
- (hlt-regexp-level-3 ((t (:background "#C847D8FEFFFF" :foreground "black"))))
- (hlt-regexp-level-4 ((t (:background "#EF47FFFFC847" :foreground "black"))))
- (hlt-regexp-level-5 ((t (:background "#FCFCE1E1FFFF" :foreground "black"))))
- (hlt-regexp-level-6 ((t (:background "#E1E1FFFFF0F0" :foreground "black"))))
- (hlt-regexp-level-7 ((t (:background "#E1E1EAEAFFFF" :foreground "black"))))
- (hlt-regexp-level-8 ((t (:background "#F6F5FFFFE1E1" :foreground "black"))))
- (ivy-minibuffer-match-face-2 ((t (:background "#5F7F5F"))))
- (ivy-minibuffer-match-face-3 ((t (:background "#7F9F7F" :foreground "black"))))
- (ivy-minibuffer-match-face-4 ((t (:background "#8FB28F" :foreground "black"))))
- (minibuffer-prompt ((t (:foreground "#F0DFAF" :height 1.0))))
- (mode-line ((t (:background "#4c7073" :foreground "#dcdccc" :box (:line-width (2 . 2) :color "#4c7073") :height 1.1))))
- (mode-line-inactive ((t (:background "#383838" :foreground "#5F7F5F" :box (:line-width (2 . 2) :color "#383838" :style flat-button) :height 1.1))))
- (org-block ((t (:extend t :background "#333333"))))
- (org-document-info-keyword ((t (:inherit shadow :height 1.3))))
- (org-document-title ((t (:inherit default :foreground "#8CD0D3" :weight bold :height 1.3))))
- (org-drawer ((t (:foreground "#f0dfaf"))))
- (org-level-1 ((t (:inherit outline-1 :extend nil :height 1.3))))
- (org-level-2 ((t (:inherit outline-2 :extend nil :height 1.1))))
- (org-level-3 ((t (:inherit default :extend nil :foreground "#7CB8BB" :slant italic :height 1.1))))
- (org-meta-line ((t (:inherit font-lock-comment-face :height 1.1))))
- (region ((t (:extend t :background "#adcff1" :foreground "black"))))
- (tab-bar ((t (:inherit nil :background "#88b090" :foreground "#2e3330" :slant italic :height 1.1))))
- (tab-bar-tab ((t (:inherit tab-bar :background "#ccdc90"))))
- (tab-bar-tab-group-current ((t (:inherit tab-bar-tab :background "#ccdc90"))))
- (tab-bar-tab-inactive ((t (:inherit tab-bar-tab :background "#88b090" :slant italic))))
- (tab-line ((t (:inherit variable-pitch :background "#2c302d" :foreground "#dcdccc" :height 0.9))))
- (tab-line-highlight ((t (:background "grey85" :foreground "black" :box (:line-width (1 . 1) :style released-button)))))
- (tab-line-tab ((t (:inherit tab-line :box (:line-width (1 . 1) :style released-button)))))
- (tab-line-tab-current ((t (:inherit tab-line-tab :background "#262626" :foreground "#dcdccc"))))
- (tab-line-tab-inactive ((t (:inherit tab-line-tab))))
- (tab-line-tab-modified ((t (:foreground "#e89393"))))
- (web-mode-html-tag-bracket-face ((t (:foreground "#8f8f8f"))))
- :config
- (setq auray/default-color '("#2b2b2b" "#8fb28f" . "#f0dfaf"))
- (load-theme 'zenburn t))
+  :custom-face
+  (cider-debug-code-overlay-face ((t (:background "grey80" :foreground "black"))))
+  (evil-search-highlight-persist-highlight-face ((t (:background "#f8f893" :foreground "black"))))
+  (font-lock-comment-face ((t (:foreground "#7F9F7F" :slant italic))))
+  (hi-aquamarine ((t (:background "aquamarine" :foreground "black"))))
+  (hi-salmon ((t (:background "light salmon" :foreground "black"))))
+  (hlt-property-highlight ((t (:background "Wheat" :foreground "black"))))
+  (hlt-regexp-level-1 ((t (:background "#FA6CC847FFFF" :foreground "black"))))
+  (hlt-regexp-level-2 ((t (:background "#C847FFFFE423" :foreground "black"))))
+  (hlt-regexp-level-3 ((t (:background "#C847D8FEFFFF" :foreground "black"))))
+  (hlt-regexp-level-4 ((t (:background "#EF47FFFFC847" :foreground "black"))))
+  (hlt-regexp-level-5 ((t (:background "#FCFCE1E1FFFF" :foreground "black"))))
+  (hlt-regexp-level-6 ((t (:background "#E1E1FFFFF0F0" :foreground "black"))))
+  (hlt-regexp-level-7 ((t (:background "#E1E1EAEAFFFF" :foreground "black"))))
+  (hlt-regexp-level-8 ((t (:background "#F6F5FFFFE1E1" :foreground "black"))))
+  (ivy-minibuffer-match-face-2 ((t (:background "#5F7F5F"))))
+  (ivy-minibuffer-match-face-3 ((t (:background "#7F9F7F" :foreground "black"))))
+  (ivy-minibuffer-match-face-4 ((t (:background "#8FB28F" :foreground "black"))))
+  (lsp-modeline-code-actions-face ((t (:inherit warning))))
+  (lsp-ui-doc-background ((t (:background "#2b2b2b"))))
+  (minibuffer-prompt ((t (:foreground "#F0DFAF" :height 1.0))))
+  (mode-line ((t (:background "#4c7073" :foreground "#dcdccc" :box (:line-width (2 . 2) :color "#4c7073") :height 1.0))))
+  (mode-line ((t (:background "#4c7073" :foreground "#dcdccc" :box (:line-width (2 . 2) :color "#4c7073") :height 1.1))))
+  (mode-line-buffer-id ((t (:foreground "#f0dfaf" :slant italic :weight bold))))
+  (mode-line-inactive ((t (:background "#383838" :foreground "#5F7F5F" :box (:line-width (2 . 2) :color "#383838" :style flat-button) :height 1.1))))
+  (org-block ((t (:extend t :background "#333333"))))
+  (org-document-info-keyword ((t (:inherit shadow :height 1.3))))
+  (org-document-title ((t (:inherit default :foreground "#8CD0D3" :weight bold :height 1.3))))
+  (org-drawer ((t (:foreground "#f0dfaf"))))
+  (org-level-1 ((t (:inherit outline-1 :extend nil :height 1.3))))
+  (org-level-2 ((t (:inherit outline-2 :extend nil :height 1.1))))
+  (org-level-3 ((t (:inherit default :extend nil :foreground "#7CB8BB" :slant italic :height 1.1))))
+  (org-meta-line ((t (:inherit font-lock-comment-face :height 1.1))))
+  (region ((t (:extend t :background "#adcff1" :foreground "black"))))
+  (tab-bar ((t (:inherit nil :background "#88b090" :foreground "#2e3330" :slant italic :height 1.1))))
+  (tab-bar-tab ((t (:inherit tab-bar :background "#ccdc90" :foreground "#3f3f3f" :box (:line-width (3 . 3) :style pressed-button) :weight bold))))
+  (tab-bar-tab-group-current ((t (:inherit tab-bar-tab :background "#ccdc90"))))
+  (tab-bar-tab-inactive ((t (:inherit tab-bar-tab :background "#88b090" :foreground "#3f3f3f" :box (:line-width (3 . 3) :style released-button) :slant normal))))
+  (tab-line ((t (:inherit variable-pitch :background "#2c302d" :foreground "#dcdccc" :height 0.9))))
+  (tab-line-highlight ((t (:background "grey85" :foreground "black" :box (:line-width (1 . 1) :style released-button)))))
+  (tab-line-tab ((t (:inherit tab-line :box (:line-width (1 . 1) :style released-button)))))
+  (tab-line-tab-current ((t (:inherit tab-line-tab :background "#262626" :foreground "#dcdccc"))))
+  (tab-line-tab-inactive ((t (:inherit tab-line-tab))))
+  (tab-line-tab-modified ((t (:foreground "#e89393"))))
+  (web-mode-html-tag-bracket-face ((t (:foreground "#8f8f8f"))))
+  :config
+  (setq auray/default-color '("#2b2b2b" "#8fb28f" . "#f0dfaf"))
+  (load-theme 'zenburn t))
 
 (add-hook 'post-command-hook (lambda ()
   (let* (
@@ -1385,20 +1390,20 @@ This command switches to browser."
 
   (add-to-list 'tramp-remote-path "/home/auray/.local/share/npm/bin/")
 
-(lsp-register-client
- (make-lsp-client
-  :new-connection (lsp-stdio-connection
-                   #'lsp-solargraph--build-command)
-  :major-modes '(ruby-mode enh-ruby-mode)
-  :priority -1
-  :multi-root lsp-solargraph-multi-root
-  :library-folders-fn (lambda (_workspace) lsp-solargraph-library-directories)
-  :server-id 'ruby-ls-remote
-  :remote? t
-  :initialized-fn (lambda (workspace)
-                    (with-lsp-workspace workspace
-                      (lsp--set-configuration
-                       (lsp-configuration-section "solargraph"))))))
+;; (lsp-register-client
+;;  (make-lsp-client
+;;   :new-connection (lsp-stdio-connection
+;;                    #'lsp-solargraph--build-command)
+;;   :major-modes '(ruby-mode enh-ruby-mode)
+;;   :priority -1
+;;   :multi-root lsp-solargraph-multi-root
+;;   :library-folders-fn (lambda (_workspace) lsp-solargraph-library-directories)
+;;   :server-id 'ruby-ls-remote
+;;   :remote? t
+;;   :initialized-fn (lambda (workspace)
+;;                     (with-lsp-workspace workspace
+;;                       (lsp--set-configuration
+;;                        (lsp-configuration-section "solargraph"))))))
 
 
   (lsp-register-client
