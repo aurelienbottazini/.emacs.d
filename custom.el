f12bf76b5048be567aa9b80e10a807e2722f1a21
Aurélien Bottazini <32635+aurelienbottazini@users.noreply.github.com>
2024-03-06 16:15:43 +0100

    colors

--- custom.el
+++ custom.el
@@ -459,4 +459,5 @@
  '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "nil" :family "Monolisa Custom"))))
  '(envrc-mode-line-error-face ((t (:inherit mode-line :inverse-video t))))
  '(envrc-mode-line-none-face ((t (:inherit mode-line))))
- '(mode-line ((t (:background "#e89393" :foreground "#ffffd7" :height 1.1)))))
+ '(mode-line ((t (:background "#e89393" :foreground "#ffffd7" :height 1.1))))
+ '(treemacs-hl-line-face ((t (:background "#f0dfaf" :foreground "#2b2b2b")))))
