ff7e4ec5fff2d72e1bf854c840195caa5ae704ad
Aurélien Bottazini <32635+aurelienbottazini@users.noreply.github.com>
2025-01-16 22:38:06 +0100

    save

--- init.el
+++ init.el
@@ -1073,7 +1073,7 @@ This command switches to browser."
 (use-package lsp-mode
   :init
   ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
-  (setq lsp-keymap-prefix "C-c l")
+  (setq lsp-keymap-prefix "M-l")
   :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
          (js2-mode . lsp)
          ;; if you want which-key integration
