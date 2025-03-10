ab22a5e526074b85e489f605753479b3130bc6b1
Aurélien Bottazini <32635+aurelienbottazini@users.noreply.github.com>
2025-03-10 07:29:07 +0100

    ruby

--- init.el
+++ init.el
@@ -420,79 +420,10 @@ cons cell (regexp . minor-mode)."
         ))
   (setq cider-repl-display-help-banner nil))
 
+(require 'aurayauray-ruby)
+
 (use-package yaml-mode
   :mode "\\.ya?ml\\'")
-
-(use-package ruby-ts-mode
-  :mode "\\.rake\\'"
-  :mode "Rakefile\\'"
-  :mode "\\.gemspec\\'"
-  :mode "\\.ru\\'"
-  :mode "Gemfile\\'"
-  :mode "Guardfile\\'"
-  :mode "Capfile\\'"
-  :mode "\\.cap\\'"
-  :mode "\\.thor\\'"
-  :mode "\\.rabl\\'"
-  :mode "Thorfile\\'"
-  :mode "Vagrantfile\\'"
-  :mode "\\.jbuilder\\'"
-  :mode "Podfile\\'"
-  :mode "\\.podspec\\'"
-  :mode "Puppetfile\\'"
-  :mode "Berksfile\\'"
-  :mode "Appraisals\\'"
-  :mode "\\.rb$"
-  :mode "ruby"
-  :config
-  (define-key ruby-ts-mode-map (kbd "C-c .") 'robe-jump)
-  (define-key ruby-ts-mode-map (kbd "C-c C-c") 'xmp)
-  (add-hook 'ruby-ts-mode-hook (defun auray-ruby-ts-mode-hook ()
-             (modify-syntax-entry ?_ "w")       ; now '_' is not considered a word-delimiter
-             ))
-  )
-
-(use-package robe
-  :after evil
-  :diminish robe-mode
-  :config
-  (add-hook 'ruby-mode-hook 'robe-mode)
-  (add-hook 'ruby-ts-mode-hook 'robe-mode)
-  (eval-after-load 'company
-  '(push 'company-robe company-backends))
-  (evil-define-key 'normal ruby-ts-mode-map (kbd "gd") 'robe-jump)
-  )
-
-(use-package ruby-mode
-  :config
-  ;; (add-hook 'ruby-mode-hook 'subword-mode)
-
-  (define-key ruby-mode-map (kbd "C-c C-c") 'xmp)
-  (define-key ruby-mode-map (kbd "C-c r") 'rspec-rerun)
-  (use-package ruby-interpolation
-    :diminish ruby-interpolation-mode)
-  (use-package ruby-end
-    :diminish ruby-end-mode
-    )
-  (use-package rspec-mode
-    :config
-    (add-hook 'after-init-hook 'inf-ruby-switch-setup) ;When you've hit the breakpoint, hit C-x C-q to enable inf-ruby
-  (define-key rspec-mode-map (kbd "C-c r") 'rspec-rerun)
-    ))
-
-(require 'rcodetools)
-(defadvice comment-dwim (around rct-hack activate)
-  "If comment-dwim is successively called, add => mark."
-  (if (and (or (eq major-mode 'enh-ruby-mode)
-               (eq major-mode 'ruby-mode)
-               (eq major-mode 'ruby-ts-mode)
-               )
-           (eq last-command 'comment-dwim))
-      (progn
-        (if (eq major-mode 'enh-ruby-mode)
-            (end-of-line))
-        (insert "=>"))
-    ad-do-it))
 
 (use-package go-mode
   :mode "\\.go\\'")
@@ -832,20 +763,6 @@ This command switches to browser."
 (setq speedbar-directory-unshown-regexp "^$")
 
 (setq project-switch-commands 'project-dired)
-
-(use-package rubocopfmt
-  :diminish rubocopfmt-mode
-  :hook
-  (ruby-ts-mode . rubocopfmt-mode)
-  (ruby-mode . rubocopfmt-mode)
-  :config
-
-  (defun silence-rubocop-messages (orig-fun &rest args)
-    (let ((inhibit-message t))
-      (apply orig-fun args)))
-
-  (advice-add 'rubocopfmt :around #'silence-rubocop-messages)
-  )
 
 
 (define-derived-mode typescriptreact-mode web-mode "TypescriptReact"
