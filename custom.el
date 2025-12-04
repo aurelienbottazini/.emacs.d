f8cb876e5f8a51992a9dd0be000cf34ec3502ccf
Aurélien Bottazini <32635+aurelienbottazini@users.noreply.github.com>
2025-12-04 08:12:49 +0100

    faces

--- custom.el
+++ custom.el
@@ -363,8 +363,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "white" :foreground "red" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Operator Mono SSm AB"))))
- '(fixed-pitch ((t (:family "Operator Mono Ssm AB"))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Operator Mono SSm AB"))))
+ '(fixed-pitch ((t (:family "Operator Mono SSm AB"))))
  '(org-block-begin-line ((t (:inherit font-lock-comment-face :extend t :slant italic))))
  '(org-block-end-line ((t (:inherit font-lock-comment-face :extend t :slant italic))))
  '(org-level-1 ((t (:inherit outline-1 :extend nil :weight bold :height 1.0))))
