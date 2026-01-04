edec965c0b5fafae16fda34cad992403ad7da860
Aurélien Bottazini <32635+aurelienbottazini@users.noreply.github.com>
2026-01-04 16:55:16 +0100

    pragmatapro

--- custom.el
+++ custom.el
@@ -369,7 +369,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Monolisa v2 variable custom"))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 150 :width normal :foundry "nil" :family "PragmataPro"))))
  '(error ((t (:background "#ffe0e0" :foreground "black" :weight bold))))
  '(fixed-pitch ((t (:family "Monolisa v2 variable custom"))))
  '(font-lock-comment-face ((t (:background "#fffabc" :foreground "black" :slant italic))))
