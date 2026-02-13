c034d1d29e6a7fd42a387f8959c68fe54a5b8cbd
Aurélien Bottazini <32635+aurelienbottazini@users.noreply.github.com>
2026-02-13 23:15:04 +0100

    cleanup

--- init.el
+++ init.el
@@ -1,11 +1,10 @@
 ;;; -*- lexical-binding: t; -*-
 
-(require 'org)
-
-(let ((init-org (expand-file-name "init.org" user-emacs-directory))
-      (init-el (expand-file-name "init-tangled.el" user-emacs-directory)))
-  (when (or (not (file-exists-p init-el))
-            (file-newer-than-file-p init-org init-el))
-    (message "Tangling init.org -> init-tangled.el")
-    (org-babel-tangle-file init-org init-el))
+;; Ultra-fast startup: load pre-tangled config directly.
+;; To retangle manually after editing init.org, run:
+;;   M-x abo/tangle-init-org
+(let ((init-el (expand-file-name "init-tangled.el" user-emacs-directory)))
+  (unless (file-exists-p init-el)
+    (require 'org)
+    (org-babel-tangle-file (expand-file-name "init.org" user-emacs-directory) init-el))
   (load-file init-el))
