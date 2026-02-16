d3f600b138fae23d0da6b4d7051903dbdc988039
Aurélien Bottazini <32635+aurelienbottazini@users.noreply.github.com>
2026-02-16 22:49:31 +0100

    speed up emacs

--- init.el
+++ init.el
@@ -1,11 +1,12 @@
 ;;; -*- lexical-binding: t; -*-
 
-(let ((init-org (expand-file-name "init.org" user-emacs-directory))
-      (init-el (expand-file-name "init-tangled.el" user-emacs-directory)))
+(let* ((init-org (expand-file-name "init.org" user-emacs-directory))
+       (init-base (expand-file-name "init-tangled" user-emacs-directory))
+       (init-el (concat init-base ".el")))
   (unless (file-exists-p init-el)
     (require 'org)
     (message "Tangling init.org -> init-tangled.el")
     (org-babel-tangle-file init-org init-el))
   (when (file-newer-than-file-p init-org init-el)
     (message "init-tangled.el is older than init.org; save init.org to re-tangle."))
-  (load init-el nil 'nomessage))
+  (load init-base nil 'nomessage))
