2f67b9de3848b3679b4801759cbc7f7a1b3d42e7
Aurélien Bottazini <32635+aurelienbottazini@users.noreply.github.com>
2026-03-24 08:20:05 +0100

    gui font size

--- custom.el
+++ custom.el
@@ -448,7 +448,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Operator Mono Ssm AB"))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 180 :width normal :foundry "nil" :family "Operator Mono Ssm AB"))))
  '(fixed-pitch ((t (:family "Operator Mono Ssm AB"))))
  '(org-drawer ((t (:inherit font-lock-comment-delimiter-face :weight normal))))
  '(org-special-keyword ((t (:inherit font-lock-comment-delimiter-face :weight normal)))))
